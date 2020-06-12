abfb55395e3bee620583f0846cd4083645107090
erikglee <erik.lee.mn@gmail.com>
2020-06-12 13:21:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setuptools.setup(
     author="Erik Lee",
     author_email="leex6144@umn.edu",
     description="A package to aid in resting-state fMRI analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/erikglee/discovery_imaging_utils",
     packages=setuptools.find_packages(),
     classifiers=[
