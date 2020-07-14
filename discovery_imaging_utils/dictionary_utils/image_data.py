f39028edce5e0a4205b8fcd30672fb895f2eb2f9
erikglee <erik.lee.mn@gmail.com>
2020-07-14 13:24:44 -0500

    add nii_ to beginning of nifti_ids

--- discovery_imaging_utils/dictionary_utils/image_data.py
+++ discovery_imaging_utils/dictionary_utils/image_data.py
@@ -243,7 +243,7 @@ def populate(lh_gii_data_path=None,
 			data = nifti_data
 
 		image_data_dict['nifti_data_inds'] = nifti_data_inds
-		image_data_dict['nifti_ids'] = nifti_ids
+		image_data_dict['nifti_ids'] = ['nii_' + str(temp_label) for temp_label in nifti_data_inds]
 
 	print(6)
 
