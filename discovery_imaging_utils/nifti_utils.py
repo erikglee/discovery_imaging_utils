f64edbe5e7cf97dec1925b2b0036a8ee26938f31
erikglee <erik.lee.mn@gmail.com>
2020-08-31 10:35:31 -0500

    update

--- discovery_imaging_utils/nifti_utils.py
+++ discovery_imaging_utils/nifti_utils.py
@@ -171,7 +171,7 @@ def parcellate_nifti(nifti_data_to_parcellate, parcellation_path, demean_before_
 
 
 	#Create array to store parcellated output
-if input_ts_matrix.ndim == 4:
+	if input_ts_matrix.ndim == 4:
 		depth = input_ts_matrix.shape[3]
 	else:
 		depth = 1
