96863577b0a07e08ea3817893fe1c108af3453bc
erikglee <erik.lee.mn@gmail.com>
2020-10-01 17:07:52 -0500

    update

--- discovery_imaging_utils/dictionary_utils/image_data.py
+++ discovery_imaging_utils/dictionary_utils/image_data.py
@@ -165,10 +165,10 @@ def convert_hdf5_to_images(hdf5_file_path, output_folder, overwrite = False):
 
 			lh_data = f['data'][f['lh_data_inds']]
 
-			if data.ndim == 1:
+			if lh_data.ndim == 1:
 				dim = 1
 			else:
-				dim = data.shape[1]
+				dim = lh_data.shape[1]
 
 			lh_gifti_data = np.zeros((f['ids/lh_ids'].attrs['lh_gifti_shape'][0], dim))
 
@@ -192,13 +192,14 @@ def convert_hdf5_to_images(hdf5_file_path, output_folder, overwrite = False):
 		#Make RH gifti if necessary
 		if 'rh_data_inds' in f.keys():
 
-			if data.ndim == 1:
-				dim = 1
-			else:
-				dim = data.shape[1]
-
 			rh_data = f['data'][f['rh_data_inds']]
 			rh_gifti_data = np.zeros((f['ids/rh_ids'].attrs['rh_gifti_shape'][0], dim))
+
+			if rh_data.ndim == 1:
+				dim = 1
+			else:
+				dim = rh_data.shape[1]
+
 
 			if 'parcel_names' in f['ids/rh_ids'].attrs.keys():
 
