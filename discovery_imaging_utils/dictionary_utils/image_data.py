fb9d44ccf410f1a24cd33d2f4fd00d2bcf29eb0d
erikglee <erik.lee.mn@gmail.com>
2020-09-30 21:49:28 -0500

    Update

--- discovery_imaging_utils/dictionary_utils/image_data.py
+++ discovery_imaging_utils/dictionary_utils/image_data.py
@@ -164,7 +164,13 @@ def convert_hdf5_to_images(hdf5_file_path, output_folder, overwrite = False):
 		if 'lh_data_inds' in f.keys():
 
 			lh_data = f['data'][f['lh_data_inds']]
-			lh_gifti_data = np.zeros((f['ids/lh_ids'].attrs['lh_gifti_shape'][0], lh_data.shape[1]))
+
+			if lh_data.ndims == 1:
+				dim = 1
+			else:
+				dim = lh_data.shape[1]
+
+			lh_gifti_data = np.zeros((f['ids/lh_ids'].attrs['lh_gifti_shape'][0], dim))
 
 
 			if 'parcel_names' in f['ids/lh_ids'].attrs.keys():
@@ -186,8 +192,13 @@ def convert_hdf5_to_images(hdf5_file_path, output_folder, overwrite = False):
 		#Make RH gifti if necessary
 		if 'rh_data_inds' in f.keys():
 
+			if rh_data.ndims == 1:
+				dim = 1
+			else:
+				dim = rh_data.shape[1]
+
 			rh_data = f['data'][f['rh_data_inds']]
-			rh_gifti_data = np.zeros((f['ids/rh_ids'].attrs['rh_gifti_shape'][0], rh_data.shape[1]))
+			rh_gifti_data = np.zeros((f['ids/rh_ids'].attrs['rh_gifti_shape'][0], dim))
 
 			if 'parcel_names' in f['ids/rh_ids'].attrs.keys():
 
