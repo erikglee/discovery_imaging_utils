fe549fc57fa8f8f007431761eb41c0e43023d90b
erikglee <erik.lee.mn@gmail.com>
2020-10-06 14:24:04 -0500

    Update

--- discovery_imaging_utils/__init__.py
+++ discovery_imaging_utils/__init__.py
@@ -6,3 +6,4 @@ from discovery_imaging_utils import imaging_visualizations
 from discovery_imaging_utils import nifti_utils
 from discovery_imaging_utils import parc_ts_dictionary
 from discovery_imaging_utils import triple_network_model
+from discovery_imaging_utils import signal_proc_utils
