4833d479d1f3b7f8574fad4876b9116fe97abdbb
erikglee <erik.lee.mn@gmail.com>
2021-04-22 13:57:34 -0500

    Update

--- discovery_imaging_utils/reports/qc/ind_structural_qc.py
+++ discovery_imaging_utils/reports/qc/ind_structural_qc.py
@@ -52,11 +52,11 @@ def construct_report(subject_path, report_path):
 
     relevant_paths = [path_to_t1, path_to_aparcaseg_dseg, path_to_csf_mask, path_to_wm_mask, path_to_gm_mask, path_to_t1_mni, path_to_harv_oxf_mni]
     print(relevant_paths)
-    for temp_path in relevant_paths:
-        if os.path.exists(temp_path) == Fase:
-            print('Missing: ' + temp_path)
-        else:
-            print('Found: ' + temp_path)
+    #for temp_path in relevant_paths:
+    #    if os.path.exists(temp_path) == Fase:
+    #        print('Missing: ' + temp_path)
+    #    else:
+    #        print('Found: ' + temp_path)
 
     print(4)
 
