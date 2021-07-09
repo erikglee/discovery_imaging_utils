9d17eb53a2a0edb115bbe3bf1baaff38b8df379b
erikglee <erik.lee.mn@gmail.com>
2021-07-09 13:50:56 -0500

    Update

--- discovery_imaging_utils/nifti_utils.py
+++ discovery_imaging_utils/nifti_utils.py
@@ -172,46 +172,46 @@ def parcellate_nifti(nifti_data_to_parcellate, parcellation_path, demean_before_
 
     #Create array to store parcellated output
     if input_ts_matrix.ndim == 4:
-    depth = input_ts_matrix.shape[3]
+        depth = input_ts_matrix.shape[3]
     else:
-    depth = 1
+        depth = 1
 
     parcellated_nifti_data = np.zeros((unique_mask_vals.shape[0], depth))
     parc_mean_signal_intensities = np.zeros(unique_mask_vals.shape[0])
     parcel_dictionary = {}
 
     if input_mask_matrix.shape[0:3] != input_ts_matrix.shape[0:3]:
-    raise NameError('Error: the first three dimensions of the input mask and input timeseries must be the same.')
+        raise NameError('Error: the first three dimensions of the input mask and input timeseries must be the same.')
 
 
     for i in range(len(unique_mask_vals)):
 
-    inds = np.where(input_mask_matrix == unique_mask_vals[i])
-    parcel_dictionary['nii_' + str(unique_mask_vals[i])] = inds
-    temp_timeseries = input_ts_matrix[inds]
+        inds = np.where(input_mask_matrix == unique_mask_vals[i])
+        parcel_dictionary['nii_' + str(unique_mask_vals[i])] = inds
+        temp_timeseries = input_ts_matrix[inds]
 
     if depth > 1:
-    voxel_means = np.nanmean(temp_timeseries, axis=1)
+        voxel_means = np.nanmean(temp_timeseries, axis=1)
     else:
-    voxel_means = temp_timeseries
+        voxel_means = temp_timeseries
 
     voxel_means[np.where(np.abs(voxel_means) < 0.000001)] = np.nan
     parcel_mean = np.nanmean(voxel_means)
 
     if demean_before_averaging:
 
-    if depth > 1:
-    temp_timeseries = temp_timeseries/voxel_means[:,None]
-    parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries, axis=0)*parcel_mean
-    else:
-    parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries)
+        if depth > 1:
+            temp_timeseries = temp_timeseries/voxel_means[:,None]
+            parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries, axis=0)*parcel_mean
+        else:
+            parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries)
 
     else:
 
-    if depth > 1:
-    parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries, axis=0)
-    else:
-    parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries)
+        if depth > 1:
+            parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries, axis=0)
+        else:
+            parcellated_nifti_data[i,:] = np.nanmean(temp_timeseries)
 
 
     unique_mask_vals = unique_mask_vals.tolist()
@@ -225,7 +225,7 @@ def incorporate_nifti_inclusion_mask(func_data, inclusion_mask_path, cutoff = 0.
     inclusion_mask_data = nib.load(inclusion_mask_path).get_fdata()
 
     if inclusion_mask_data.shape[0:3] != func_data.shape[0:3]:
-    raise NameError('Error: the first three dimensions of the input mask and input timeseries must be the same.')
+        raise NameError('Error: the first three dimensions of the input mask and input timeseries must be the same.')
 
     inds_to_include = np.where(inclusion_mask_data > cutoff)
     inds_to_exclude = np.where(inclusion_mask_data <= cutoff)
