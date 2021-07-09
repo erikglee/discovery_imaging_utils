c0e131a16dc8605288f25fad93b37d758d94f9fa
erikglee <erik.lee.mn@gmail.com>
2021-07-09 13:32:58 -0500

    Update

--- discovery_imaging_utils/stats_utils.py
+++ discovery_imaging_utils/stats_utils.py
@@ -875,78 +875,69 @@ def optimal_SVHT_coef(num_dimensions, num_observations):
         return med
 
 
-    def incMarPas(x0,B):
+	def incMarPas(x0,B):
 
-        topSpec = (1 + np.sqrt(B))**2;
-        botSpec = (1 - np.sqrt(B))**2;
-
-        #print(topSpec)
-        #print(botSpec)
+		topSpec = (1 + np.sqrt(B))**2;
+		botSpec = (1 - np.sqrt(B))**2;
 
 
 
-        def IfElse(Q,point):
-            y = point;
-            return y
 
-        def MarPas(x, topSpec, botSpec):
+		def IfElse(Q,point):
+			y = point;
+			return y
 
-            #print((np.multiply(topSpec-x, x-botSpec) > 0).shape)
-            #print(np.sqrt(np.divide(np.multiply(topSpec-x, x-botSpec),np.multiply(B, x)/(2 * np.pi))).shape)
+		def MarPas(x, topSpec, botSpec):
 
-            temp = IfElse(np.multiply(topSpec-x, x-botSpec) > 0,
-                          np.divide(np.sqrt(np.multiply(topSpec-x, x-botSpec)),B*x*2*np.pi))
+			temp = IfElse(np.multiply(topSpec-x, x-botSpec) > 0,
+						np.divide(np.sqrt(np.multiply(topSpec-x, x-botSpec)),B*x*2*np.pi))
 
-            #print(temp)
-            return(temp)
+			return(temp)
 
-        #print(MarPas(x0,topSpec,botSpec))
-        #print(np.multiply(topSpec-x0, x0-botSpec) > 0)
-        #print(np.divide(np.sqrt(np.multiply(topSpec-x0, x0-botSpec)),B*x0*2*np.pi))
 
-        I = quad(MarPas, x0, topSpec, args = (topSpec, botSpec))[0]
+		I = quad(MarPas, x0, topSpec, args = (topSpec, botSpec))[0]
 
-        return I
+		return I
 
-    B = np.min([num_dimensions, num_observations])/np.max([num_dimensions, num_observations])
-    return optimal_SVHT_coef_sigma_unknown(B)
+	B = np.min([num_dimensions, num_observations])/np.max([num_dimensions, num_observations])
+	return optimal_SVHT_coef_sigma_unknown(B)
 
-    def pca_denoise(data):
-    '''Function that uses PCA to denoise data
+	def pca_denoise(data):
+	'''Function that uses PCA to denoise data
 
-    This function assumes that the data matrix
-    has some low-rank feature set + gaussian
-    noise. Given that, this function uses SVD
-    to decompose the matrix, then reconstructs
-    the matrix using a subset of the components
-    from SVD/PCA with the goal of retaining the
-    low-rank features without the noise.
+	This function assumes that the data matrix
+	has some low-rank feature set + gaussian
+	noise. Given that, this function uses SVD
+	to decompose the matrix, then reconstructs
+	the matrix using a subset of the components
+	from SVD/PCA with the goal of retaining the
+	low-rank features without the noise.
 
-    See Gavish and Donoho 2014 for reference.
+	See Gavish and Donoho 2014 for reference.
 
-    Parameters
-    ----------
+	Parameters
+	----------
 
-    data : numpy.ndarray
-        A 2-d array with low rank data + noise
+	data : numpy.ndarray
+		A 2-d array with low rank data + noise
 
-    Returns
-    -------
+	Returns
+	-------
 
-    cleaned : numpy.ndarrray
-        A denoised version of the data array with
-        the same dimensions
+	cleaned : numpy.ndarrray
+		A denoised version of the data array with
+		the same dimensions
 
-    num_good_svs : int
-        The estimated rank used for reconstruction
+	num_good_svs : int
+		The estimated rank used for reconstruction
 
-    '''
+	'''
 
-    u, s, vh = scipy.linalg.svd(data, full_matrices = False)
-    y = np.diagonal(data).copy()
-    omega = optimal_SVHT_coef(data.shape[0], data.shape[1])
-    cutoff = omega * np.median(s)
-    num_good_svs = np.max(np.where(s > cutoff))
-    cleaned = u[:,:(num_good_svs + 1)] @ np.diag(s[:(num_good_svs+1)]) @ vh[:(num_good_svs+1),:]
+	u, s, vh = scipy.linalg.svd(data, full_matrices = False)
+	y = np.diagonal(data).copy()
+	omega = optimal_SVHT_coef(data.shape[0], data.shape[1])
+	cutoff = omega * np.median(s)
+	num_good_svs = np.max(np.where(s > cutoff))
+	cleaned = u[:,:(num_good_svs + 1)] @ np.diag(s[:(num_good_svs+1)]) @ vh[:(num_good_svs+1),:]
 
-    return cleaned, num_good_svs
+	return cleaned, num_good_svs
